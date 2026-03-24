8efc8b0aefc2422601a99d60bd2bf8532c5db020
Victor Kauã <victorelezeu@gmail.com>
2026-03-24 01:46:44 -0300

    repositories

---
+++ Tarefas/src/main/java/com/Gerenciamento_Tarefas/Tarefas/repositories/TarefaRepository.java
@@ -0,0 +1,10 @@
+package com.Gerenciamento_Tarefas.Tarefas.repositories;
+
+import com.Gerenciamento_Tarefas.Tarefas.models.Tarefa;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TarefaRepository extends JpaRepository<Tarefa, Long> {
+}
+
